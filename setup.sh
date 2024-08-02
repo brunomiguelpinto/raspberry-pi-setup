a423fdc6bd591bb5bae264ed093b2c30d509c43a
Miguel Pinto <darkymiguel@gmail.com>
2024-08-02 17:29:05 +0100

    added docker

--- setup.sh
+++ setup.sh
@@ -19,6 +19,20 @@ check_vim() {
     fi
 }
 
+# Function to check if Docker is installed
+check_docker() {
+    if [ -x "$(command -v docker)" ]; then
+        echo "Docker is already installed."
+    else
+        echo "Installing Docker..."
+        curl -fsSL https://get.docker.com -o get-docker.sh
+        sudo sh get-docker.sh
+        # Add your user to the Docker group
+        sudo usermod -aG docker ${USER}
+    fi
+}
+
 # Begin script execution
 update_system         # Update and clean the system first
 check_vim             # Install Vim if not installed
+check_docker          # Install Docker if not installed
