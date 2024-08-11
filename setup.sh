3c56c289c03ff0ce4093f7f553a8a463891ecb4e
Miguel Pinto <darkymiguel@gmail.com>
2024-08-11 15:19:10 +0100

    test

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status.
+
 # Define update and cleanup functions
 update_system() {
     echo "Updating system packages..."
@@ -29,6 +31,9 @@ check_docker() {
         sudo sh get-docker.sh
         # Add your user to the Docker group
         sudo usermod -aG docker ${USER}
+        echo "Docker installed. Please log out and log back in to apply Docker group changes."
+        echo "After logging back in, re-run this script to complete the setup."
+        exit 1  # Exit script to allow user to log out and back in
     fi
 }
 
